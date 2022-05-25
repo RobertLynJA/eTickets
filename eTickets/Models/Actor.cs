9d40c68a11f1caf1954996a08093a5702f4d857e
Robert Lyn <lenny4422@gmail.com>
2022-05-24 21:38:24 -0400

    Added Actor Model

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [AllowNull]
+        public string ProfilePictureURL { get; set; }
+
+        [AllowNull]
+        public string FullName { get; set; }
+
+        [AllowNull]
+        public string Bio { get; set; }
+    }
+}
