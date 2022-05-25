d45b363eeeb959d9a6989a4b3b732a80880a3fbf
Robert Lyn <lenny4422@gmail.com>
2022-05-24 21:55:29 -0400

    Added Cinema Model

---
+++ eTickets/Models/Cinema.cs
@@ -0,0 +1,17 @@
+﻿namespace eTickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [AllowNull]
+        public string Logo { get; set; }
+
+        [AllowNull]
+        public string Name { get; set; }
+
+        [AllowNull]
+        public string Description { get; set; }
+    }
+}
