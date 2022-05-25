49dcbe1a2aec9c4d4f586d438487ad22a18d45c0
Robert Lyn <lenny4422@gmail.com>
2022-05-24 21:52:52 -0400

    Added Producer Model

---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,17 @@
+﻿namespace eTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [AllowNull]
+        public string ProfilePictureURL { get; set; }
+
+        [AllowNull]
+        public string FullName { get; set; }
+
+        [AllowNull]
+        public string Bio { get; set; }
+    }
+}
